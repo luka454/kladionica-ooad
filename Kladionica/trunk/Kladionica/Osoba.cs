32f5194ef5348cce858c80f7ce841902f3964fce
aploco1@etf.unsa.ba <aploco1@etf.unsa.ba@7d26e288-af10-a122-ec4e-853439413b4d>
2014-04-08 21:29:26 +0000

    bazna klasa osoba

---
+++ Kladionica/trunk/Kladionica/Osoba.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kladionica
+{
+    public class Osoba
+    {
+        protected int _ID;
+        protected string _ime;
+        protected string _prezime;
+        protected string _username;
+        protected int _hashPassword;
+
+        public int DajPassword
+        {
+            get { return _hashPassword; }
+            set { _hashPassword = value; }
+        }
+
+        public string DajUsername
+        {
+            get { return _username; }
+            set { _username = value; }
+        }
+
+        public string DajPrezime
+        {
+            get { return _prezime; }
+            set { _prezime = value; }
+        }
+
+        public string DajIme
+        {
+            get { return _ime; }
+            set { _ime = value; }
+        }
+
+        public int DajID
+        {
+            get { return _ID; }
+            set { _ID = value; }
+        }
+    }
+}
