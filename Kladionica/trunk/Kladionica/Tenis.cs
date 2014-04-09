2ef7747d6bee8adc3355c62007632adc68621c30
elmirhodzic93@gmail.com <elmirhodzic93@gmail.com@7d26e288-af10-a122-ec4e-853439413b4d>
2014-04-09 23:39:41 +0000

    V1.0: Administracija, Transakcija, Tenis

---
+++ Kladionica/trunk/Kladionica/Tenis.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kladionica
+{
+    public class Tenis : Igra
+    {
+        /// <summary>
+        /// dodaj tipove za tenis
+        /// </summary>
+        public String PrviProtivnik { get; set; }
+        public String DrugiProtivnik { get; set; }
+        public int PrviPoenaSetova { get; set; }
+        public int DrugiPoenaSetova { get; set; }
+        public Boolean provjeriTip(String tip) { return false; }
+        public Boolean procjeriJeLiDobitni(String tip) { return false; }
+
+    }
+}
---
+++ Kladionica/trunk/Kladionica/Transakcija.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kladionica
+{
+    public class Transakcija 
+    {
+	    public int ID;
+	    public DateTime vrijeme ;
+	    public Decimal Iznos ;
+	    public ClanKluba KojiKorisnik ;
+    }
+
+}
