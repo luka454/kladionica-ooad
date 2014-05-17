4e758215a11491af5cc9a3cc87bc7d7ecb5f1cf0
elmirhodzic93@gmail.com <elmirhodzic93@gmail.com@7d26e288-af10-a122-ec4e-853439413b4d>
2014-05-17 22:01:43 +0000

    Koeficijent, Tenis, Tiket, Transakcija volume 1 commit

--- Kladionica/trunk/KladionicaKlase/Koeficijent.cs
+++ Kladionica/trunk/KladionicaKlase/Koeficijent.cs
@@ -9,10 +9,18 @@ namespace KladionicaKlase
     {
         public String tip;
         public float koeficijent;
+        private bool dobitni;
         public Koeficijent(string t, float k)
         {
             tip = t;
             koeficijent = k;
+            dobitni = false;
         }
+        public bool Dobitni
+        {
+            get { return dobitni; }
+            set { dobitni = value; }
+        }
+        
     }
 }
--- Kladionica/trunk/KladionicaKlase/Tenis.cs
+++ Kladionica/trunk/KladionicaKlase/Tenis.cs
@@ -12,14 +12,26 @@ namespace KladionicaKlase
         public String DrugiProtivnik { get; set; }
         public int PrviPoenaSetova { get; set; }
         public int DrugiPoenaSetova { get; set; }
-        public Tenis(int id, DateTime p, string n, StatusIgre si, string pp, string dp, int pps, int dps): base(id,p,n,si)
+        public int PrviPoenaGemova { get; set; }
+        public int DrugiPoenaGemova { get; set; }
+        public Tenis(int id, DateTime p, string n, StatusIgre si, string pp, string dp, int pps, int dps, int ppg, int dpg): base(id,p,n,si)
         {
             PrviProtivnik = pp;
             DrugiProtivnik = dp;
             PrviPoenaSetova = pps;
             DrugiPoenaSetova = dps;
+            PrviPoenaGemova = ppg;
+            DrugiPoenaGemova = dpg;
         }
-        public override bool ProvjeriTip(String tip) { throw new Exception("Unimplementerd"); }
-        public override bool ProvjeriJeLiDobitni(String tip) { throw new Exception("Unimplementerd"); }
+        public override bool ProvjeriTip(String tip) 
+        {
+            foreach (Koeficijent k in koeficijenti) if (tip == k.tip) return true;
+            return false;
+        }
+        public override bool ProvjeriJeLiDobitni(String tip) 
+        {
+            foreach (Koeficijent k in koeficijenti) if (tip == k.tip) return k.Dobitni;
+            throw new Exception("Nije validan tip!");
+        }
     }
 }
